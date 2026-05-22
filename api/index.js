e5af7265e7e3736848524c9e900ee52cb809bab1
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-22 09:29:54 -0300

    chore: update shareCode to ULNM92NVPEJ

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=BXCGWK8WMOI&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=ULNM92NVPEJ&afp=oa' });
     res.end();
   }
